190d9ce78516b54be693ec8533510359d1b067b1
Zach Kaiser <zachkaiser@Zachs-MacBook-Pro.local>
2025-06-11 12:19:42 -0400

    finished the homepage styling

--- src/components/homePage/index.tsx
+++ src/components/homePage/index.tsx
@@ -12,6 +12,19 @@ function HomePage(){
 
     const nameRef = useRef<HTMLHeadingElement | null>(null);
 
+    const containerVariants = {
+        hidden: {},
+        visible: {
+          transition: {
+            staggerChildren: 0.2,
+          },
+        },
+      };
+      
+      const iconVariants = {
+        hidden: { opacity: 0, x: -25 },
+        visible: { opacity: 1, x: 0, transition: { duration: 0.5, ease: "easeOut" } },
+      };
 
     useEffect(() => {
         const nameEl = nameRef.current;
@@ -35,42 +48,84 @@ function HomePage(){
     return(
         <div>
             <Container className="hero-section">
-                <div className="hero-content">
-                    <motion.div
-                        initial={{ opacity: 0, y: -50 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.95 }}
-                    >
-                        <h2 className="title">Hi. My name is,</h2>
-                        <h1 className="name" ref={nameRef}>Zach Kaiser</h1>
-                        <h3 className="studentText">Software Engineering Student</h3>
-                    </motion.div>
-                    <motion.div
-                        initial={{ opacity: 0, y: 50 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.5 }}
-                    >
-                        <p className="summary">
-                            Motivated Software Engineering student with a passion for designing software solutions. 
-                            Looking to learn new skills every day!
-                        </p>
-                        <Row className="button-section">
-                            <Col className="contact-col" xs="auto">
-                                <motion.div whileHover={{scale: 1.1}}>
-                                    <Button className="contact-button" variant="outline-primary">
-                                        <Link to='/contact'>Contact me</Link>
-                                    </Button>
-                                </motion.div>
-                            </Col>
-                            <Col className="projects-col" xs="auto">
-                                <motion.div whileHover={{scale: 1.1}}>
-                                    <Button className="projects-button" variant="outline-primary">
-                                        <Link to='/projects'>View my work</Link>
-                                    </Button>
-                                </motion.div>
-                            </Col>
-                        </Row>
-                    </motion.div>
+                <div className="content-wrapper">
+                    <div className="hero-content">
+                        <motion.div
+                            initial={{ opacity: 0, y: -50 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.95 }}
+                        >
+                            <h2 className="title">Hi. My name is,</h2>
+                            <h1 className="name" ref={nameRef}>Zach Kaiser</h1>
+                            <h3 className="studentText">Software Engineering Student</h3>
+                        </motion.div>
+                        <motion.div
+                            initial={{ opacity: 0, y: 50 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.5 }}
+                        >
+                            <p className="summary">
+                                Motivated Software Engineering student with a passion for designing software solutions. 
+                                Looking to learn new skills every day!
+                            </p>
+                            <Row className="button-section">
+                                <Col className="contact-col" xs="auto">
+                                    <motion.div whileHover={{scale: 1.1}}>
+                                        <Button className="contact-button" variant="outline-primary">
+                                            <Link to='/contact'>Contact me</Link>
+                                        </Button>
+                                    </motion.div>
+                                </Col>
+                                <Col className="projects-col" xs="auto">
+                                    <motion.div whileHover={{scale: 1.1}}>
+                                        <Button className="projects-button" variant="outline-primary">
+                                            <Link to='/projects'>View my work</Link>
+                                        </Button>
+                                    </motion.div>
+                                </Col>
+                            </Row>
+                        </motion.div>
+                    </div>
+                    <div className="socials">
+                        <motion.div 
+                        variants={containerVariants} 
+                        initial="hidden" 
+                        animate="visible"
+                        className="social-container"
+                        >
+                            <motion.a 
+                                href="mailto:zachwkaiser@gmail.com"
+                                variants={iconVariants}
+                                whileHover={{ scale: 1.2 }}
+                                transition={{ type: "spring", stiffness: 300 }}
+                                className="contact"
+                            >
+                                <Envelope color="white" size={40} />
+                            </motion.a>
+                            <motion.a 
+                                href="https://www.linkedin.com/in/zachwkaiser/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                variants={iconVariants}
+                                whileHover={{ scale: 1.2 }}
+                                transition={{ type: "spring", stiffness: 300 }}
+                                className="contact"
+                            >
+                                <Linkedin color="white" size={40} />
+                            </motion.a>
+                            <motion.a 
+                                href="https://github.com/zachwkaiser"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                variants={iconVariants}
+                                whileHover={{ scale: 1.2 }}
+                                transition={{ type: "spring", stiffness: 300 }}
+                                className="contact"
+                            >
+                                <Github color="white" size={40} />
+                            </motion.a>
+                        </motion.div>
+                    </div>
                 </div>
                 <motion.div
                     initial={{ opacity: 0, x: 50 }}
@@ -80,35 +135,6 @@ function HomePage(){
                 >
                     <img src={headshot} alt="My Headshot" className="headshot-img"/>
                 </motion.div>
-            </Container>
-                
-            <Container className="socials">
-                <motion.a 
-                    href="mailto:zachwkaiser@gmail.com"
-                    whileHover={{ scale: 1.05 }}
-                    className="contact"
-                >
-                    <Envelope color="white" size={40} />
-                </motion.a>
-                <motion.a 
-                    href="https://www.linkedin.com/in/zachwkaiser/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    whileHover={{ scale: 1.05, color: "#00bcd4" }}
-                    className="contact"
-                >
-                    <Linkedin color="white" size={40} />
-                </motion.a>
-                <motion.a 
-                    href="https://github.com/zachwkaiser"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    whileHover={{ scale: 1.05 }}
-                    className="contact"
-                >
-                    <Github color="white" size={40} />
-                </motion.a>
-
             </Container>
         </div>
     );
